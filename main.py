d0511102bd3408ea1bd82583e8434cebebb8d60a
Cody Jeffries <jcody@pdx.edu>
2021-05-29 19:29:34 -0700

    flushed out argparse and console menu functionality. More functionality to be added as features are enabled.

--- main.py
+++ main.py
@@ -1,28 +1,56 @@
 import argparse
+import sounddevice as sd
+import soundfile as sf
+import numpy as np
+import consolemenu
 
-def main():
+def menu():
+    print('1. Record')
+
+def play_sound_file(filename):
+    """
+    Attempts to play a sound file given a filename.
+    :param filename: str
+        .wav file
+    :return: N/A no return
+    """
+    # Extract data and sampling rate from file 
+    data, fs = sf.read(filename)
+    sd.play(data, fs)
+    sd.wait()  # Wait until file is done playing
+
+def main(args):
+    menu = ['Record', 'Play', 'Filter', 'Save']
+    if args.play:
+        play_sound_file(args.play)
+    else:
+        selection = consolemenu.SelectionMenu.get_selection(menu)
+        print(menu[selection])
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Audio Effects',
-                                    description='Simple Audio Effects'
-                                    ' program for sound and music',
-                                    allow_abbrev=False)
+                                description='Simple Audio Effects'
+                                ' program for sound and music',
+                                allow_abbrev=False)
     parser.add_argument('-r', '--record',
-                        type=str,
-                        default='temp_file')
+                    type=str,
+                    default='temp_file',
+                    help='Audio filename to save recording')
     parser.add_argument('-f', '--file',
-                        type=str)
+                    type=str,
+                    help='new file name to save audio')
     parser.add_argument('-af', '--apply_filter',
-                        type=str)
+                    type=str,
+                    help='Filter you want to apply to audio file')
     parser.add_argument('-p', '--play',
-                        type=str)
+                    type=str,
+                    help='Audio file you want to play')
 
     args = parser.parse_args()
 
-    main(args)
-
     try:
-        main()
+        main(args)
     except KeyboardInterrupt:
         print('Goodbye!')
