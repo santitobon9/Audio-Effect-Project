e54a004cffd69dca9fc1ec27ab4fbbb1d1493aa2
Cody Jeffries <jcody@pdx.edu>
2021-06-06 22:19:30 -0700

    implemented bandpass filter. Removed consolemenu. Added some plotting of signals.

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import argparse
 import sounddevice as sd
 import soundfile as sf
 import numpy as np
-import consolemenu
-
-def menu():
-    print('1. Record')
+import matplotlib.pyplot as plt
+from scipy.signal import butter, sosfilt, convolve, sosfiltfilt
+from scipy.io.wavfile import write
 
 def play_sound_file(filename):
     """
@@ -19,14 +18,33 @@ def play_sound_file(filename):
     sd.play(data, fs)
     sd.wait()  # Wait until file is done playing
 
-def main(args):
-    menu = ['Record', 'Play', 'Filter', 'Save']
-    if args.play:
-        play_sound_file(args.play)
-    else:
-        selection = consolemenu.SelectionMenu.get_selection(menu)
-        print(menu[selection])
+def bandpass_filter(data, fs):
+    fs = fs  # Sample rate
+    seconds = 1  # Duration of recording
+    nyq = 0.1 * fs
+    # print(nyq)
+    frequency_low = 1 / nyq
+    frequency_high = 500 / nyq
+    sos = butter(5, [frequency_low, frequency_high], btype='bandpass', output='sos')
+    #y = sosfilt(sos, data)
+    y = sosfiltfilt(sos, data)
+    return y, fs
 
+def main(args):
+    data, sample_rate = sf.read(args.load_file)
+    play_sound_file(args.load_file)
+    # plt.plot(data)
+    # plt.show()
+    # print('Max: ', max(data), 'Min: ', min(data))
+    data, sample_rate = bandpass_filter(data, sample_rate)
+    # autocorr = fftconvolve(data, data[::-1], mode='full')
+    # print('Max: ', max(data), 'Min: ', min(data))
+    # time = np.linspace(0, 1 * 2 * np.pi, sample_rate)
+    sd.play(data, sample_rate)
+    sd.wait()
+    plt.plot(data)
+    plt.show()
+    write('testing.wav', sample_rate, data)
 
 
 if __name__ == '__main__':
@@ -36,8 +54,11 @@ if __name__ == '__main__':
                                 allow_abbrev=False)
     parser.add_argument('-r', '--record',
                     type=str,
-                    default='temp_file',
+                    default='temp-file',
                     help='Audio filename to save recording')
+    parser.add_argument('-lf', '--load_file',
+                    type=str,
+                    help='Audio file to load')
     parser.add_argument('-f', '--file',
                     type=str,
                     help='new file name to save audio')
